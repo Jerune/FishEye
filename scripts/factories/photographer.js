4e43f6a0ccd13f0994fd73e05bb5e4271d216b3e
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-04-15 11:12:20 +0200

    Add Portrait images + Load json data

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,33 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country,tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+        
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const location = document.createElement( 'p' );
+        location.textContent = `${city}, ${country}`;
+
+        const tagLine = document.createElement( 'p' );
+        tagLine.textContent = tagline;
+
+        const dayPrice = document.createElement( 'p' );
+        dayPrice.textContent = `${price}€/jour`;
+        
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tagLine);
+        article.appendChild(dayPrice);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
